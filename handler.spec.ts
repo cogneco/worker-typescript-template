8b2d6fc7c88c5de4e6cd266bd0e849f5f8610f13
Florian Ebmeier <florian.ebmeier@payfunc.com>
2021-01-07 16:34:20 +0100

    Changed to jest
    eslint plugin

--- handler.spec.ts
+++ handler.spec.ts
@@ -1,15 +1,15 @@
-import { describe, it } from "mocha"
-import { expect } from "chai"
+import * as http from "cloud-http"
 import { handleRequest } from "./handler"
-import "./_setup"
+
+import "isomorphic-fetch"
 
 describe("handler returns response with request method", () => {
 	const methods = ["GET", "HEAD", "POST", "PUT", "DELETE", "CONNECT", "OPTIONS", "TRACE", "PATCH"]
 	methods.forEach(method => {
 		it(method, async () => {
-			const result = await handleRequest(new Request("/", { method }))
-			const text = await result.text()
-			expect(text).to.include(method)
+			const result = await handleRequest(http.Request.from(new Request("http://example.com/", { method })))
+			const text = await result.body
+			expect(text).toMatch(method)
 		})
 	})
 })
