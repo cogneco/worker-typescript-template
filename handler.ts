63dbb6810fa250452beb3698bf6d268a87a3b897
Florian Ebmeier <florian.ebmeier@payfunc.com>
2021-01-07 16:34:20 +0100

    Working template

--- handler.ts
+++ handler.ts
@@ -1,3 +1,10 @@
-export async function handleRequest(request: Request): Promise<Response> {
-	return new Response(`request method: ${request.method}`)
+import * as http from "cloud-http"
+import { router } from "./router"
+
+export async function handleRequest(request: http.Request): Promise<http.Response.Like> {
+	const result = {
+		body: `request method: ${request.method}`,
+	}
+	return result
 }
+router.add(["CONNECT", "DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT", "TRACE"], "test/", handleRequest)
